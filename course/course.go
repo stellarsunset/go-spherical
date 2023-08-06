e1b81c089a194b0fbb8ab08a9e04215c2c0f09a0
Alex Cramer <alexandercramer6@gmail.com>
2023-08-06 00:59:50 -0400

    More Course methods and tests

--- course/course.go
+++ course/course.go
@@ -14,9 +14,30 @@ a helicopter flies in a direction is it's pointed.
 */
 package course
 
+import (
+	"math"
+	"stellarsunset/spherical/spherical"
+)
+
 type Course struct {
 	angle float64
 	unit  Unit
+}
+
+func North() *Course {
+	return OfDegrees(0)
+}
+
+func South() *Course {
+	return OfDegrees(180)
+}
+
+func East() *Course {
+	return OfDegrees(90)
+}
+
+func West() *Course {
+	return OfDegrees(270)
 }
 
 func Of(angle float64, unit Unit) *Course {
@@ -31,6 +52,86 @@ func OfRadians(angle float64) *Course {
 	return &Course{angle, Radians}
 }
 
+func AngleBetween(one, two *Course) *Course {
+	return OfDegrees(spherical.AngleDifference(one.InDegrees(), two.InDegrees()))
+}
+
 func (this *Course) NativeUnit() Unit {
 	return this.unit
+}
+
+func (this *Course) In(desiredUnit Unit) float64 {
+	if this.unit == desiredUnit {
+		return this.angle
+	} else {
+		return this.angle * (UnitsPerDegree(desiredUnit) / UnitsPerDegree(this.unit))
+	}
+}
+
+func (this *Course) InDegrees() float64 {
+	return this.In(Degrees)
+}
+
+func (this *Course) InRadians() float64 {
+	return this.In(Radians)
+}
+
+func (this *Course) Negate() *Course {
+	return Of(-1*this.angle, this.unit)
+}
+
+func (this *Course) Abs() *Course {
+	return Of(math.Abs(this.angle), this.unit)
+}
+
+func (this *Course) IsPositive() bool {
+	return this.angle > 0
+}
+
+func (this *Course) IsNegative() bool {
+	return this.angle < 0
+}
+
+func (this *Course) IsZero() bool {
+	return this.angle == 0
+}
+
+func (this *Course) Times(scalar float64) *Course {
+	return Of(this.angle*scalar, this.unit)
+}
+
+func (this *Course) Plus(that *Course) *Course {
+	return Of(this.angle+that.In(this.unit), this.unit)
+}
+
+func (this *Course) Minus(that *Course) *Course {
+	return Of(this.angle+that.In(this.unit), this.unit)
+}
+
+func (this *Course) IsLessThan(that *Course) bool {
+	return this.angle < that.In(this.unit)
+}
+
+func (this *Course) IsLessThanOrEqualTo(that *Course) bool {
+	return this.angle <= that.In(this.unit)
+}
+
+func (this *Course) IsGreaterThan(that *Course) bool {
+	return this.angle > that.In(this.unit)
+}
+
+func (this *Course) IsGreaterThanOrEqualTo(that *Course) bool {
+	return this.angle >= that.In(this.unit)
+}
+
+func (this *Course) Sin() float64 {
+	return math.Sin(this.InRadians())
+}
+
+func (this *Course) Cos() float64 {
+	return math.Cos(this.InRadians())
+}
+
+func (this *Course) Tan() float64 {
+	return math.Tan(this.InRadians())
 }
--- course/course_test.go
+++ course/course_test.go
@@ -6,6 +6,10 @@ import (
 	"testing"
 )
 
+const (
+	tolerance = 0.0000001
+)
+
 func isTrue(t *testing.T, condition bool, s string) {
 	if !condition {
 		t.Error(s)
@@ -24,14 +28,129 @@ func isEqual(t *testing.T, expected, actual crs.Course) {
 	}
 }
 
-func withinError(t *testing.T, expected, actual, maxError float64, s string) {
-	if math.Abs(expected-actual) > maxError {
-		t.Errorf("%s: want = %f, got = %f, tol = %f", s, expected, actual, maxError)
+func withinError(t *testing.T, expected, actual float64, s string) {
+	if math.Abs(expected-actual) > tolerance {
+		t.Errorf("%s: want = %f, got = %f, tol = %f", s, expected, actual, tolerance)
 	}
 }
 
-func withinFractionOfExpected(t *testing.T, expected, actual, percentError float64, s string) {
-	if math.Abs(expected-actual) > (percentError * expected) {
-		t.Errorf("%s: want = %f, got = %f, tol = %f", s, expected, actual, percentError)
+func withinFractionOfExpected(t *testing.T, expected, actual float64, s string) {
+	if math.Abs(expected-actual) > (tolerance * expected) {
+		t.Errorf("%s: want = %f, got = %f, tol = %f", s, expected, actual, tolerance)
 	}
+}
+
+func TestOf(t *testing.T) {
+
+	oneDegree, twoRadians := crs.Of(1, crs.Degrees), crs.Of(2, crs.Radians)
+
+	withinError(t, 1., oneDegree.InDegrees(), "InDegrees(OneDegree)")
+	isTrue(t, oneDegree.NativeUnit() == crs.Degrees, "NativeUnit(OneDegree)")
+
+	withinError(t, 2., twoRadians.InRadians(), "InRadians(TwoRadians)")
+	isTrue(t, twoRadians.NativeUnit() == crs.Radians, "NativeUnit(TwoRadians)")
+}
+
+func TestOfDegrees(t *testing.T) {
+
+	oneDegree := crs.OfDegrees(1)
+
+	withinError(t, 1., oneDegree.InDegrees(), "InDegrees(OneDegree)")
+	isTrue(t, oneDegree.NativeUnit() == crs.Degrees, "NativeUnit(OneDegree)")
+}
+
+func TestOfRadians(t *testing.T) {
+
+	twoRadians := crs.OfRadians(2)
+
+	withinError(t, 2., twoRadians.InRadians(), "InRadians(TwoRadians)")
+	isTrue(t, twoRadians.NativeUnit() == crs.Radians, "NativeUnit(TwoRadians)")
+}
+
+func TestUnitConversions(t *testing.T) {
+
+	ninetyDegrees := crs.OfDegrees(90.)
+	withinError(t, math.Pi/2., ninetyDegrees.InRadians(), "InRadians(90)")
+
+	threeSixtyDegrees := crs.OfDegrees(360)
+	withinError(t, math.Pi*2, threeSixtyDegrees.InRadians(), "InRadians(360)")
+
+	zeroDegrees := crs.OfDegrees(0)
+	withinError(t, 0., zeroDegrees.InRadians(), "InRadians(0)")
+
+	twoRadians := crs.OfRadians(2)
+	withinError(t, 2*(180/math.Pi), twoRadians.InDegrees(), "InDegrees(2rad)")
+
+	piRadians := crs.OfRadians(math.Pi)
+	withinError(t, 180., piRadians.InDegrees(), "InDegrees(Pirad)")
+
+	zeroRadians := crs.OfRadians(0)
+	withinError(t, 0, zeroRadians.InDegrees(), "InDegrees(0rad)")
+}
+
+func TestUnitConversionsNegatives(t *testing.T) {
+
+	ninetyDegrees := crs.OfDegrees(-90.)
+	withinError(t, -math.Pi/2., ninetyDegrees.InRadians(), "InRadians(90)")
+
+	threeSixtyDegrees := crs.OfDegrees(-360)
+	withinError(t, -math.Pi*2, threeSixtyDegrees.InRadians(), "InRadians(360)")
+
+	zeroDegrees := crs.OfDegrees(0)
+	withinError(t, 0., zeroDegrees.InRadians(), "InRadians(0)")
+
+	twoRadians := crs.OfRadians(-2)
+	withinError(t, -2*(180/math.Pi), twoRadians.InDegrees(), "InDegrees(2rad)")
+
+	piRadians := crs.OfRadians(-math.Pi)
+	withinError(t, -180., piRadians.InDegrees(), "InDegrees(Pirad)")
+
+	zeroRadians := crs.OfRadians(0)
+	withinError(t, 0, zeroRadians.InDegrees(), "InDegrees(0rad)")
+}
+
+func TestNegate(t *testing.T) {
+	ninetyDegrees, negativeNinetyDegrees := crs.OfDegrees(90), crs.OfDegrees(-90)
+	isEqual(t, *ninetyDegrees, *negativeNinetyDegrees.Negate())
+	isEqual(t, *negativeNinetyDegrees, *ninetyDegrees.Negate())
+}
+
+func TestAbs(t *testing.T) {
+	ninetyDegrees, negativeNinetyDegrees := crs.OfDegrees(90), crs.OfDegrees(-90)
+	isEqual(t, *ninetyDegrees, *ninetyDegrees.Abs())
+	isEqual(t, *ninetyDegrees, *negativeNinetyDegrees.Abs())
+}
+
+func TestIsPositive(t *testing.T) {
+	isTrue(t, crs.OfDegrees(20).IsPositive(), "IsPositive(20)")
+	isFalse(t, crs.OfDegrees(0).IsPositive(), "IsPositive(0)")
+	isFalse(t, crs.OfDegrees(-20).IsPositive(), "IsPositive(-20)")
+}
+
+func TestIsNegative(t *testing.T) {
+	isFalse(t, crs.OfDegrees(20).IsNegative(), "IsNegative(20)")
+	isFalse(t, crs.OfDegrees(0).IsNegative(), "IsNegative(0)")
+	isTrue(t, crs.OfDegrees(-20).IsNegative(), "IsNegative(-20)")
+}
+
+func TestIsZero(t *testing.T) {
+	isFalse(t, crs.OfDegrees(20).IsZero(), "IsZero(20)")
+	isTrue(t, crs.OfDegrees(0).IsZero(), "IsZero(0)")
+	isFalse(t, crs.OfDegrees(-20).IsZero(), "IsZero(-20)")
+}
+
+func TestTimes(t *testing.T) {
+
+}
+
+func TestPlus(t *testing.T) {
+
+}
+
+func TestMinus(t *testing.T) {
+
+}
+
+func TestComparisonMethods(t *testing.T) {
+
 }
